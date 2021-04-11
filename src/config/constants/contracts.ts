089298a7c983cc85c0f493388cb125b860c8f1b0
yielddeer <80870883+yielddeer@users.noreply.github.com>
2021-04-11 16:20:42 +0800

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,10 +1,10 @@
 export default {
   cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-    97: '',
+    56: '0x34e57430bbee5503ff7ed3c18384cedc0355be38',
+    97: '0x34e57430bbee5503ff7ed3c18384cedc0355be38',
   },
   masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0x75214442857ff7ACA44a9eb2E8481F1968a4Fc49',
     97: '',
   },
   wbnb: {
